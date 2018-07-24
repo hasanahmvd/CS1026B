ea6898f65e2f6c67e8848e935fb6a70fc7ba3919
Hasan Ahmed <30710041+hasanahmed98@users.noreply.github.com>
2018-07-24 01:37:03 -0400

    Add files via upload

---
+++ Labs/Lab1/Task3/cdbh.py
@@ -0,0 +1,3 @@
+print("My favourite song is Summer Girl")
+print("My favourite movie is Never Back Down")
+print("My favourite day of the year is March 30")
---
+++ Labs/Lab1/Task3/task.py
@@ -0,0 +1,10 @@
+# Replace placeholders with correct Python
+
+print("My first python code")
+answer1 = 46*2
+answer2 = 3+5
+
+print(answer1)
+print(answer2)
+
+print("Everything works")
