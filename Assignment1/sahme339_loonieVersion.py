c6b45faa088796916a4239def57c9e022b188093
hasanahmed98 <30710041+hasanahmed98@users.noreply.github.com>
2018-07-23 23:26:44 -0400

    Add files via upload

---
+++ Assignment1/sahme339_loonieVersion.py
@@ -0,0 +1,34 @@
+# This program computes the reward of a carnival game where a loonie is tossed and reward is given if it does not lands on a line
+from random import random # import random so random number can be generated later
+
+#Ask user for distance and reward and store as float variables
+distance = float(input("Please enter the distance between lines (in mm): "))
+reward = float(input("Please enter the 'reward' if the customer wins (in $): "))
+
+#Define variables including counter
+upperLimit = float(3*distance)
+hits = 0
+
+#Define constants, radius of loonie
+RADIUS = 13.25
+
+#Set for loop which plays 1000 times and checks if the radius of loonie is on randomly generated to fall on the lines
+for i in range(999):
+    on_line = False
+    x = random() * upperLimit #Generates where the coin is randomly placed on x axis
+    y = random() * upperLimit #Generates where the coin is randomly placed on y axis
+    for k in range(3):
+        if(abs((k*distance)-x) <= 13.25): #Checks to see if the x component of where it lands is within coins radius
+            on_line = True
+        elif(abs((k*distance)-y) <= 13.25): #Checks to see if the y component of where it lands is also within coins radius
+            on_line = True
+    if(not(on_line)):
+        hits += 1 #Counter is increased with every coin toss
+
+payout = 1000 - (hits * reward) #Defines the monetary outcome of the 1000 coin tosses
+
+#Determines if the owner made or lost money with the 1000 tosses and outputs the appropriate statement
+if (payout >= 0):
+    print("For 1000 loonie tosses, you may get ${:.2f}".format(payout))
+if (payout < 0):
+    print("For 1000 loonie tosses, you may lose ${:.2f}".format(abs(payout)))
---
+++ Assignment1/test.py
@@ -0,0 +1,5 @@
+for i in range(10): # loop 10 times
+print(random()) #print a random number between 0 and 1
+print(10*random()) #print a random number between 0 and 10
+print() # print a blank line
+print("Done")
