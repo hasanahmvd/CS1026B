6405237dd0c3006381a027f7a1218d903ecaa3c6
Hasan Ahmed <30710041+hasanahmed98@users.noreply.github.com>
2018-07-24 01:40:30 -0400

    Add files via upload

---
+++ Labs/Lab2/Task2/task.py
@@ -0,0 +1,15 @@
+# Try the following Python and answer the questions
+
+x = 2
+print(2<5)
+print(x<=2)
+print(2!=5)
+print(x==2)# 2 equals signs checks equality of something one equals sign is an assignment operator
+
+#try printing 2=2
+#delete the # in the line below to uncomment and test it
+print("2=2")
+
+print((x+3)<7)
+#add your own!
+print((x+1)>8)
